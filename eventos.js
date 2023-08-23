8b1f352b199eff9057601dfb38938e1b0a870302
[MelaniePorley] <melanie.porley@gmail.com>
2023-08-23 20:46:48 -0300

    solution-click added

--- eventos.js
+++ eventos.js
@@ -1,7 +1,13 @@
 const div = document.querySelector("div");
+const btn = document.querySelector("button");
 
 div.addEventListener("click", function() {
     return alert("Hola! Soy el div");
+});
+
+btn.addEventListener("click", (event) => {
+        event.stopPropagation();
+        return alert("Hola!");
 });
 
 
